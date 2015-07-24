6d6967f11872530db5c6d9d886ba05134a5b77dd
pankaj-dhami <reachpankajdhami@gmail.com>
2015-07-24 17:00:27 +0530

    update

--- AngularWebApi/Scripts/app/productList-controller.js
+++ AngularWebApi/Scripts/app/productList-controller.js
@@ -10,18 +10,26 @@
 
 (function () {
 
-    var app = angular.module('myApp', []);
-    app.controller('productsCtrl', function ($scope, $http) {
+    var app = angular.module('myApp', ['ngProgressLite']);
+    app.config(['ngProgressLiteProvider', function (ngProgressLiteProvider) {
+        ngProgressLiteProvider.settings.ease = 'ease-in';
+    }]);
+    app.controller('productsCtrl', function ($scope, $http, ngProgressLite, $timeout) {
+        ngProgressLite.start();
         $http.get("http://localhost:25344/api/Products")
-        .success(function (response) { $scope.products = response; });
+        .success(function (response) {
+            $scope.products = response;
+            ngProgressLite.done();
+        });
         $scope.toggle = function () {
             $scope.myVar = !$scope.myVar;
         }
         $scope.postFormData = function () {
-
-            $http.put("http://localhost:25344/api/Products/" + $scope.productPrice, { ProductName: $scope.productName, ProductCode: $scope.productName, Price: $scope.productPrice })
+            ngProgressLite.start();
+            $http.post("http://localhost:25344/api/Products/", { ProductName: $scope.productName, ProductCode: $scope.productName, Price: $scope.productPrice })
             .success(function (data) {
                 $scope.products = data;
+                ngProgressLite.done();
             });
         }
     });
